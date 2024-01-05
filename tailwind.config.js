a1770c0a063572bb3c5871e88a2ba2fbe1a4c2ac
Michael Troya <mtroya@benchsci.com>
2024-01-04 22:16:55 -0500

    Add theme and top loader

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
+  content: ['./src/**/*.{ts,tsx}'],
   prefix: '',
   theme: {
     container: {
       center: true,
       padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
+      screens: { '2xl': '1400px' },
     },
     extend: {
       colors: {
